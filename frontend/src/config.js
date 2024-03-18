1d80f9e80f73bc21e5f848cdf92152c62e1724f9
garbuzshay <shay.garbuz@e.braude.ac.il>
2024-03-18 19:52:17 +0200

    another try

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,14 +1,6 @@
-// const API_BASE_URL = process.env.BACKEND_URL || "http://localhost:5000";
-
-// const config = {
-//   apiBaseUrl: API_BASE_URL + "/api",
-// };
-
-// export default config;
-
 const config = {
-  // apiBaseUrl: process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000/api',
-  apiBaseUrl: 'https://recipe-platform-react.vercel.app/api'
+  apiBaseUrl: process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000/api',
+  // apiBaseUrl: 'https://recipe-platform-react.vercel.app/api'
 };
 
 export default config;
