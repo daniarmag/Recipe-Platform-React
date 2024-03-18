cafb81f2ae96ee4bc135418fb21fcc7a0a33e2e5
garbuzshay <shay.garbuz@e.braude.ac.il>
2024-03-18 12:24:45 +0200

    styles

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,43 @@
+// import express from 'express';
+// import cors from 'cors';
+// import bodyParser from 'body-parser';
+// import recipeRoutes from './routes/recipesRoutes.js';
+// import usersRoutes from './routes/usersRoutes.js';
+// import config from './config.js';
+
+
+
+// // Unhandled exception handler
+// process.on('uncaughtException', (error) => {
+//   console.error('Uncaught Exception:', error.message, error.stack);
+//   // Optionally, you might want to perform additional cleanup or logging here
+//   // process.exit(1); // Terminate the process after handling the exception
+// });
+
+
+
+// const app = express();
+// app.use(bodyParser.json({ limit: '50mb' }));
+// app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+// // Serve frontend files
+// app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+// app.use(cors());
+// // Routes
+// app.use('/api', recipeRoutes);
+// app.use('/api/users', usersRoutes);
+
+// // Serve frontend for any other routes
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+// });
+
+// app.listen(config.port, () =>
+//   console.log(`Server is live @ ${config.hostUrl} on port ${config.port}`),
+// );
+
+// const path = require('path');
+
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
@@ -18,18 +58,12 @@ process.on('uncaughtException', (error) => {
 const app = express();
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-// Serve frontend files
-app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 app.use(cors());
 // Routes
 app.use('/api', recipeRoutes);
 app.use('/api/users', usersRoutes);
 
-// Serve frontend for any other routes
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
-});
 
 app.listen(config.port, () =>
   console.log(`Server is live @ ${config.hostUrl} on port ${config.port}`),
