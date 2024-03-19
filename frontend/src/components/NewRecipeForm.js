6dcfe6d2bbe47000397cf1c19bc817555f253434
May Caspi <maycas@gmail.com>
2024-03-19 20:49:00 +0200

    another style

--- frontend/src/components/NewRecipeForm.js
+++ frontend/src/components/NewRecipeForm.js
@@ -1,238 +1,288 @@
-import React, { useState } from 'react';
-import RecipesApi from '../api/RecipesApi';
-import { useTheme } from '../context/ThemeContext';
-import { useAuth } from '../context/AuthContext';
+import React, { useState } from "react";
+import RecipesApi from "../api/RecipesApi";
+import { useTheme } from "../context/ThemeContext";
+import { useAuth } from "../context/AuthContext";
 import usePopupMessage from "../hooks/usePopupMessage.js";
 import PopupMessage from "./PopupMessage.js";
 
 const CreateNewRecipe = () => {
-    const { user } = useAuth();
-    const [recipeData, setRecipeData] = useState({
-      author: user?.email || '',
-      name: '',
-      description: '',
-      ingredients: '',
-      preparation: '',
-      nutritionalValues: {
-        calories: '',
-        fat: '',
-        proteins: ''
-      },
-      image: null,
-    });
-  
+  const { user } = useAuth();
+  const [recipeData, setRecipeData] = useState({
+    author: user?.email || "",
+    name: "",
+    description: "",
+    ingredients: "",
+    preparation: "",
+    nutritionalValues: {
+      calories: "",
+      fat: "",
+      proteins: "",
+    },
+    image: null,
+  });
 
-    const { isVisible: isPopupVisible, message, showPopup } = usePopupMessage(); // Use the custom hook for popup message
+  const { isVisible: isPopupVisible, message, showPopup } = usePopupMessage(); // Use the custom hook for popup message
 
+  const handleInputChange = (e) => {
+    const { id, value } = e.target;
+    if (id.includes("nutritionalValues.")) {
+      const key = id.split(".")[1];
+      setRecipeData({
+        ...recipeData,
+        nutritionalValues: { ...recipeData.nutritionalValues, [key]: value },
+      });
+    } else {
+      setRecipeData({ ...recipeData, [id]: value });
+    }
+  };
+  const [imagePreviewUrl, setImagePreviewUrl] = useState(null);
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setRecipeData({ ...recipeData, image: file });
 
-    const handleInputChange = (e) => {
-      const { id, value } = e.target;
-      if (id.includes('nutritionalValues.')) {
-        const key = id.split('.')[1];
-        setRecipeData({
-          ...recipeData,
-          nutritionalValues: { ...recipeData.nutritionalValues, [key]: value },
-        });
-      } else {
-        setRecipeData({ ...recipeData, [id]: value });
-      }
-    };
-  
-    const handleImageChange = (e) => {
-      const file = e.target.files[0];
-      console.log(file)
-      if (file) {
-        setRecipeData({ ...recipeData, image: file });
-      }
-    };
-  
-    const handleSubmit = async (e) => {
-      e.preventDefault();
-      const formData = new FormData();
-      console.log(recipeData)
-      formData.append("author", recipeData.author); 
-      formData.append("name", recipeData.name);
-      formData.append("description", recipeData.description);
-      formData.append("ingredients", recipeData.ingredients);
-      formData.append("preparation", recipeData.preparation);
-      formData.append("calories", recipeData.nutritionalValues.calories);
-      formData.append("fat", recipeData.nutritionalValues.fat);
-      formData.append("proteins", recipeData.nutritionalValues.proteins);
-    
-      // Assuming `recipeData.image` is a File object from an <input type="file" />
-      if (recipeData.image) {
-        formData.append("image", recipeData.image);
-      }
-    
-      try {
-        // Call the createRecipe method from RecipesApi and pass the recipeData
-        const response = await RecipesApi.createRecipe(formData);
-        console.log('Recipe successfully added:', response);
-        showPopup(`Recipe added successfully`);
-        // alert('Recipe added successfully');
+      // Set file reader to read the image file as a data URL
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        // Update the state with the image preview URL
+        setImagePreviewUrl(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
 
-        setRecipeData({
-          author: user?.email || '',
-          name: '',
-          description: '',
-          ingredients: '',
-          preparation: '',
-          nutritionalValues: {
-            calories: '',
-            fat: '',
-            proteins: ''
-          },
-          image: null,
-        });
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    console.log(recipeData);
+    formData.append("author", recipeData.author);
+    formData.append("name", recipeData.name);
+    formData.append("description", recipeData.description);
+    formData.append("ingredients", recipeData.ingredients);
+    formData.append("preparation", recipeData.preparation);
+    formData.append("calories", recipeData.nutritionalValues.calories);
+    formData.append("fat", recipeData.nutritionalValues.fat);
+    formData.append("proteins", recipeData.nutritionalValues.proteins);
 
-        // Optionally, clear the form or navigate the user to a different page
-      } catch (error) {
-        console.error('Failed to add the recipe:', error);
-        alert('Failed to add the recipe');
-      }
-    };
+    // Assuming `recipeData.image` is a File object from an <input type="file" />
+    if (recipeData.image) {
+      formData.append("image", recipeData.image);
+    }
 
+    try {
+      // Call the createRecipe method from RecipesApi and pass the recipeData
+      const response = await RecipesApi.createRecipe(formData);
+      console.log("Recipe successfully added:", response);
+      showPopup(`Recipe added successfully`);
+      // alert('Recipe added successfully');
 
-    const { theme } = useTheme();
-    const { darkMode } = theme;
+      setRecipeData({
+        author: user?.email || "",
+        name: "",
+        description: "",
+        ingredients: "",
+        preparation: "",
+        nutritionalValues: {
+          calories: "",
+          fat: "",
+          proteins: "",
+        },
+        image: null,
+      });
 
-    const submitBtnStyle = darkMode === 'dark' ? 'bg-slate-600' : 'bg-green-500'
+      // Optionally, clear the form or navigate the user to a different page
+    } catch (error) {
+      console.error("Failed to add the recipe:", error);
+      alert("Failed to add the recipe");
+    }
+  };
 
+  const { theme } = useTheme();
+  const { darkMode } = theme;
+
+  const submitBtnStyle = darkMode === "dark" ? "bg-slate-600" : "bg-green-500";
 
   return (
-    <div className="container mx-auto" >
-        <PopupMessage isVisible={isPopupVisible} message={message} onClose={showPopup} duration={3000} />
-        <div className="text-3xl text-gray-800 font-bold text-center py-3 bigTitle sm:text-3xl mt-3 mb-3">
-          ADD YOUR RECIPE HERE
-        </div>
-        <form className="max-w-md mx-auto pb-4" onSubmit={handleSubmit}>
-  
-        <div className="mb-4 mx-4"> {/* Added mx-4 for spacing */}
-          <label htmlFor="name" className="block text-sm font-semibold text-gray-700">
+    <div className="container mx-auto AssistantFont">
+      <PopupMessage
+        isVisible={isPopupVisible}
+        message={message}
+        onClose={showPopup}
+        duration={3000}
+      />
+      <div className="text-3xl  text-gray-800 font-bold text-center py-3 bigTitle sm:text-3xl mt-3 mb-3 ">
+        ADD YOUR RECIPE HERE
+      </div>
+      <form className="max-w-md mx-auto pb-4" onSubmit={handleSubmit}>
+        <div className="mb-4 mx-2">
+          <label
+            htmlFor="name"
+            className="block text-lg font-semibold text-gray-700 mb-1"
+          >
             RECIPE NAME
           </label>
           <textarea
             id="name"
-            className="border rounded p-2 text-sm text-left w-full"
+            className="p-2 text-sm text-left w-full placeholder-gray-400 resize-none"
             placeholder="Enter Recipe Name"
             value={recipeData.name}
             onChange={handleInputChange}
+            style={{ borderRadius: "0.999rem", backgroundColor: "#f5f5f5" }} // TailwindCSS rounded-md equivalent and light gray background
           />
         </div>
-        
 
-      <div className="mb-4 mx-4"> {/* Added mx-4 for spacing */}
-        <label htmlFor="description" className="block text-sm font-semibold text-gray-700">
-          DESCRIPTION
-        </label>
-        <textarea
-          id="description"
-          className="border rounded p-2 text-sm text-left w-full"
-          placeholder="Enter Description"
-          value={recipeData.description}
-          onChange={handleInputChange}
-        />
-      </div>
-
-      <div className="mb-4 mx-4"> {/* Added mx-4 for spacing */}
-        <label htmlFor="ingredients" className="block text-sm font-semibold text-gray-700">
-          INGREDIENTS
-        </label>
-        <textarea
-          id="ingredients"
-          className="border rounded p-2 text-sm text-left w-full"
-          placeholder="Enter Ingredients. Each ingredient on a separate line."
-          value={recipeData.ingredients}
-          onChange={handleInputChange}
-        />
-      </div>
-
-      <div className="mb-4 mx-4"> {/* Added mx-4 for spacing */}
-        <label htmlFor="preparation" className="block text-sm font-semibold text-gray-700">
-          PREPARATION
-        </label>
-        <textarea
-          id="preparation"
-          className="border rounded p-2 text-sm text-left w-full"
-          placeholder="Enter Preparation. Each step on a separate line."
-          value={recipeData.preparation}
-          onChange={handleInputChange}
-        />
-      </div>
-
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-        <div className="mb-2 mx-4"> {/* Added mx-4 for spacing */}
-          <label htmlFor="calories" className="block text-sm font-semibold text-gray-700">
-            CALORIES
+        <div className="mb-4  mx-2">
+          {" "}
+          {/* Added mx-4 for spacing */}
+          <label
+            htmlFor="description"
+            className="block text-lg font-semibold text-gray-700"
+          >
+            DESCRIPTION
           </label>
           <textarea
-            id="nutritionalValues.calories"
-            className="border rounded p-2 text-sm text-left w-full"
-            placeholder="Enter Calories"
-            value={recipeData.nutritionalValues.calories}
+            id="description"
+            className="p-2 text-sm text-left w-full placeholder-gray-400 resize-none"
+            placeholder="Enter Description"
+            value={recipeData.description}
             onChange={handleInputChange}
+            style={{ borderRadius: "0.999rem", backgroundColor: "#f5f5f5" }} // TailwindCSS rounded-md equivalent and light gray background
           />
         </div>
 
-        <div className="mb-2 mx-4"> {/* Added mx-4 for spacing */}
-          <label htmlFor="fat" className="block text-sm font-semibold text-gray-700">
-            FAT
+        <div className="mb-4 mx-2">
+          {" "}
+          {/* Added mx-4 for spacing */}
+          <label
+            htmlFor="ingredients"
+            className="block text-lg font-semibold text-gray-700"
+          >
+            INGREDIENTS
           </label>
           <textarea
-            id="nutritionalValues.fat"
-            className="border rounded p-2 text-sm text-left w-full"
-            placeholder="Enter Fat (g)"
-            value={recipeData.nutritionalValues.fat}
+            id="ingredients"
+            className="p-2 text-sm text-left w-full placeholder-gray-400 resize-none"
+            placeholder="Enter Ingredients. Each ingredient on a separate line."
+            value={recipeData.ingredients}
             onChange={handleInputChange}
+            style={{ borderRadius: "0.999rem", backgroundColor: "#f5f5f5" }} // TailwindCSS rounded-md equivalent and light gray background
           />
         </div>
 
-        <div className="mb-2 mx-4"> {/* Added mx-4 for spacing */}
-          <label htmlFor="proteins" className="block text-sm font-semibold text-gray-700">
-            PROTEINS
+        <div className="mb-4 mx-2">
+          {" "}
+          {/* Added mx-4 for spacing */}
+          <label
+            htmlFor="preparation"
+            className="block text-lg font-semibold text-gray-700"
+          >
+            PREPARATION
           </label>
           <textarea
-            id="nutritionalValues.proteins"
-            className="border rounded p-2 text-sm text-left w-full"
-            placeholder="Enter Proteins (g)"
-            value={recipeData.nutritionalValues.proteins}
+            id="preparation"
+            className="p-2 text-sm text-left w-full placeholder-gray-400 resize-none"
+            placeholder="Enter Preparation. Each step on a separate line."
+            value={recipeData.preparation}
             onChange={handleInputChange}
+            style={{ borderRadius: "0.999rem", backgroundColor: "#f5f5f5" }} // TailwindCSS rounded-md equivalent and light gray background
           />
         </div>
-      </div>
 
-              {/* Image input */}
-              <div className="mb-4 mx-4 ">
-                <label htmlFor="recipeImage" className="block text-sm font-semibold text-gray-700 ">
-                  Upload Image
-                </label>
-                <input
-                  type="file"
-                  id="recipeImage"
-                  name="recipeImage"
-                  accept="image/*"
-                  className="hidden"
-                  onChange={handleImageChange}
-                />
-                <p className="text-gray-700 px-4 font-semibold ">{recipeData.image && `Image uploaded: ${recipeData.image}`}</p>
-                <button
-                  type="button"
-                  onClick={() => document.getElementById('recipeImage').click()}
-                  className="cursor-pointer bg-gray-200 text-gray-700 px-4 py-2 rounded-md"
-                >
-                  Choose File
-                </button>
-              </div>
+        <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
+          <div className="mb-2 mx-2">
+            {" "}
+            {/* Added mx-4 for spacing */}
+            <label
+              htmlFor="calories"
+              className="block text-lg font-semibold text-gray-700"
+            >
+              CALORIES
+            </label>
+            <textarea
+              id="nutritionalValues.calories"
+              className=" rounded p-2 text-sm text-left w-full resize-none"
+              placeholder="Enter Calories"
+              value={recipeData.nutritionalValues.calories}
+              onChange={handleInputChange}
+              style={{ borderRadius: "0.999rem", backgroundColor: "#f5f5f5" }} // TailwindCSS rounded-md equivalent and light gray background
+            />
+          </div>
 
-              {/* Submit button */}
-              <button
-                type="submit"
-                // style={submitBtnStyle}
-                className={`text-lg font-bold p-3 ${submitBtnStyle} rounded-md shadow-md  mx-auto block w-full text-white`}
-              >
-                SUBMIT RECIPE
-              </button>
-            </form>
+          <div className="mb-2 mx-2">
+            {" "}
+            {/* Added mx-4 for spacing */}
+            <label
+              htmlFor="fat"
+              className="block text-lg font-semibold text-gray-700"
+            >
+              FAT
+            </label>
+            <textarea
+              id="nutritionalValues.fat"
+              className="p-2 text-sm text-left w-full placeholder-gray-400 resize-none"
+              placeholder="Enter Fat (g)"
+              value={recipeData.nutritionalValues.fat}
+              onChange={handleInputChange}
+              style={{ borderRadius: "0.999rem", backgroundColor: "#f5f5f5" }} // TailwindCSS rounded-md equivalent and light gray background
+            />
+          </div>
+
+          <div className="mb-2 mx-2">
+            {" "}
+            {/* Added mx-4 for spacing */}
+            <label
+              htmlFor="proteins"
+              className="block text-lg font-semibold text-gray-700"
+            >
+              PROTEINS
+            </label>
+            <textarea
+              id="nutritionalValues.proteins"
+              className="p-2 text-sm text-left w-full placeholder-gray-400 resize-none"
+              placeholder="Enter Proteins (g)"
+              value={recipeData.nutritionalValues.proteins}
+              onChange={handleInputChange}
+              style={{ borderRadius: "0.999rem", backgroundColor: "#f5f5f5" }} // TailwindCSS rounded-md equivalent and light gray background
+            />
+          </div>
+        </div>
+
+        {/* Image input */}
+        <div className="mb-6 mx-2">
+          <input
+            type="file"
+            id="recipeImage"
+            name="recipeImage"
+            accept="image/*"
+            className="hidden"
+            onChange={handleImageChange}
+          />
+          {/* Image preview */}
+          {imagePreviewUrl && (
+            <img
+              src={imagePreviewUrl}
+              alt="Preview"
+              className="w-full h-64 object-cover rounded-md" // Adjust w-24 h-24 to control the size of the preview
+            />
+          )}
+          <button
+            type="button"
+            onClick={() => document.getElementById("recipeImage").click()}
+            className="cursor-pointer text-lg bg-gray-300 text-gray-700 px-4 py-2 mt-4 rounded-md font-bold hover:bg-gray-400"
+          >
+            Choose File
+          </button>
+        </div>
+
+        {/* Submit button */}
+        <button
+          type="submit"
+          // style={submitBtnStyle}
+          className={`text-lg font-bold py-3 ${submitBtnStyle} rounded-lg shadow-md mx-2 w-full text-white hover:bg-emerald-500`}
+        >
+          SUBMIT RECIPE
+        </button>
+      </form>
     </div>
   );
 };
--- frontend/src/pages/AddRecipe.js
+++ frontend/src/pages/AddRecipe.js
@@ -1,23 +1,24 @@
-import React from 'react';
-import CreateNewRecipe from '../components/NewRecipeForm'; // Replace with the correct path
-import { useTheme } from '../context/ThemeContext';
-import Navbar from '../components/Navbar';
-
+import React from "react";
+import CreateNewRecipe from "../components/NewRecipeForm"; // Replace with the correct path
+import { useTheme } from "../context/ThemeContext";
+import Navbar from "../components/Navbar";
 
 function AddRecipePage() {
   const { theme } = useTheme();
   const { darkMode } = theme;
 
-  const pageBackgroundColor = darkMode === 'dark' ? 'DarkGray' : 'white';
+  const pageBackgroundColor = darkMode === "dark" ? "DarkGray" : "white";
 
   return (
-    
-   <div style={{ backgroundColor: pageBackgroundColor }}>
-                    <Navbar />
+    <div
+      style={{ backgroundColor: pageBackgroundColor }}
+      className="min-h-screen"
+    >
+      <Navbar />
       {/* Other content of your AddRecipePage if any */}
       <CreateNewRecipe />
     </div>
   );
-};
+}
 
 export default AddRecipePage;
