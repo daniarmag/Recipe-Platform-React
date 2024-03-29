77b7e3819aebd6f48df830fcd8083438a39891ca
garbuzshay <shay.garbuz@e.braude.ac.il>
2024-03-29 12:37:57 +0300

    fixed the 'x' buttons and make the modal in the center of the screen

--- frontend/src/components/Modal.js
+++ frontend/src/components/Modal.js
@@ -6,31 +6,33 @@ import React from "react";
 const Modal = ({ recipe, closeModal, addToPlanner }) => {
   const mealOptions = ["Breakfast", "Lunch", "Dinner"];
   return (
-    <div className="modal bg-white w-44 p-4 rounded-lg shadow-md fixed top-1/2 left-0 z-10">
-      <button
-        className="close-modal-btn absolute top-2 left-2"
-        onClick={closeModal}
-      >
-        x
-      </button>
-
-      <div className="text-center text-lg font-bold title mb-2 top">
-        {`Add ${recipe.name} to:`}
-      </div>
-
-      {mealOptions.map((meal) => (
+    <div className="modal-container">
+      <div className="modal bg-white p-4 rounded-lg shadow-md fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10">
         <button
-          key={meal}
-          className="meal-option-btn text-green-500 py-1 px-2 mt-1 text-xs border border-green-500 px-3 rounded-full w-full max-w-full text-ellipsis whitespace-no-wrap AssistantFont font-semibold"
-          onClick={() => {
-            console.log(recipe);
-            addToPlanner(meal, recipe);
-            closeModal();
-          }}
+          onClick={closeModal}
+          className="absolute top-0 right-0 mr-1 text-gray-800 text-2xl font-bold bg-transparent cursor-pointer"
         >
-          {meal.charAt(0).toUpperCase() + meal.slice(1)}
+          &times;
         </button>
-      ))}
+
+        <div className="text-center text-lg font-bold title mb-2 top">
+          {`Add ${recipe.name} to:`}
+        </div>
+
+        {mealOptions.map((meal) => (
+          <button
+            key={meal}
+            className="meal-option-btn text-green-500 py-1  mt-1 text-s border border-green-500 px-3 rounded-full w-full max-w-full text-ellipsis whitespace-no-wrap AssistantFont font-semibold"
+            onClick={() => {
+              console.log(recipe);
+              addToPlanner(meal, recipe);
+              closeModal();
+            }}
+          >
+            {meal.charAt(0).toUpperCase() + meal.slice(1)}
+          </button>
+        ))}
+      </div>
     </div>
   );
 };
--- frontend/src/components/RecipePopup.js
+++ frontend/src/components/RecipePopup.js
@@ -17,10 +17,16 @@ const RecipePopup = ({ title, imageSrc, description, ingredients, nutritionalVal
   );
 
   return (
-<div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-4 rounded-md shadow-lg z-50 text-center w-10/12 max-w-md mx-auto sm:w-3/5 md:w-3/5 lg:w-1/2 xl:w-1/3 max-h-[22]  popup">
-      <button onClick={onClose} className="absolute top-2 left-2">
-        <span className="text-stone-900 font-extrabold text-2xl bg-slate-200 rounded cursor-pointer">&times;</span>
-      </button>
+<div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 border  bg-white p-4 rounded-md shadow-lg z-50 text-center w-10/12 max-w-md mx-auto sm:w-3/5 md:w-3/5 lg:w-1/2 xl:w-1/3 max-h-[22]  popup">
+
+
+    <button
+        onClick={onClose}
+        className="absolute top-0 right-0  text-gray-800 text-2xl font-bold bg-transparent cursor-pointer"
+      >
+        &times; 
+    </button>
+
       <img src={imageSrc} alt={title} className="w-full h-48 object-cover rounded-lg mb-1" />
       <h2 className="text-lg font-semibold mb-1">{title}</h2>
       <p className="text-gray-600 font-normal mb-1">{description}</p>
